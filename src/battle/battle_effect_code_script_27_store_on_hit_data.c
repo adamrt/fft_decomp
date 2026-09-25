2ce3dd20d601e05e07d0e6852966417f30666333
Adam Patterson <adam@adamrt.com>
2026-09-24 23:37:15 -0700

    Use typed effect geometry alias

--- src/battle/battle_effect_code_script_27_store_on_hit_data.c
+++ src/battle/battle_effect_code_script_27_store_on_hit_data.c
@@ -2,9 +2,8 @@
 #include "fft/effect.h"
 
 s32 battle_effect_code_script_27_store_on_hit_data(effect_record_t* effect) {
-    battle_effect_store_first_section_of_on_hit_data(
-        (battle_effect_on_hit_vector_t*)(g_effect_particle_system_data + 4));
-    battle_effect_set_inertia_threshold(*(s32*)(g_effect_particle_system_data + 0x10));
+    battle_effect_store_first_section_of_on_hit_data((battle_effect_on_hit_vector_t*)g_effect_geometry_table->gravity);
+    battle_effect_set_inertia_threshold(g_effect_geometry_table->inertia_threshold);
     effect->pc += 2;
     return EFFECT_SCRIPT_RESULT_CONTINUE;
 }
--- src/battle/battle_effect_set_particle_system_data_pointer.c
+++ src/battle/battle_effect_set_particle_system_data_pointer.c
@@ -6,5 +6,5 @@ void battle_effect_set_particle_system_data_pointer(s16 effect_id) {
     effect_file_header_t* header;
 
     header = g_battle_effect_data_ptrs[effect_id];
-    g_effect_particle_system_data = (u8*)header + header->particle_system;
+    g_effect_geometry_table = (effect_geometry_table_t*)((u8*)header + header->particle_system);
 }
